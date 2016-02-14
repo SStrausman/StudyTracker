d43be3afdce771bfe2b63dd974dba3333fcb350a
shea <sstrausman@gmail.com>
2016-02-14 01:23:46 -0600

    added basic crud functionality to study

--- app/controllers/studies_controller.rb
+++ app/controllers/studies_controller.rb
@@ -1,7 +1,24 @@
 class StudiesController < ApplicationController
 
 	def index
-		
+
+	end
+
+	def show
+
+	end
+
+	def create
+
+	end
+
+	def update
+	end
+
+	private
+
+	def study_params
+		params.require(:study).permit(:title, :principal_investigator, :open)
 	end
 
 end
