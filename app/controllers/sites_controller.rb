8d15413a883080ea3e1cb17ce4716da33f7b22e3
shea <sstrausman@gmail.com>
2016-02-14 07:38:41 -0600

    ability to register site added

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -1,16 +1,20 @@
 class SitesController < ApplicationController
 
-	def index
-	end
-
 	def show
+		@site = Site.find(params[:id])
 	end
 
 	def new
+		@site = Site.new
 	end
 
 	def create
-
+		@site = Site.new(site_params)
+		if @site.save
+			redirect_to site_path(@site)
+		else
+    	render :new
+		end
 	end
 
 	def update
